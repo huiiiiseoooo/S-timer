a18fcb7b934ee1ee32194992ee09a44422d11136
huiiiiseoooo <sshs1552@naver.com>
2026-06-26 19:03:13 +0900

    chore: springboot 기초 세팅 java version 17

---
+++ src/main/java/com/choishingong/s_timer/STimerApplication.java
@@ -0,0 +1,14 @@
+package com.choishingong.s_timer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class STimerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(STimerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/choishingong/s_timer/STimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choishingong.s_timer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class STimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
